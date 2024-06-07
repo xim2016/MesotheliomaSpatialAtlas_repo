08e7f16964838b3955715a1181878e96e4d0b3a2
xim2016 <xj_ma@yahoo.com>
2024-06-07 11:35:44 -0400

    test plotly_chart

--- utils.py
+++ utils.py
@@ -228,7 +228,7 @@ def show_plotly_image(img_file, height=750):
                                         'drawrect',
                                         'eraseshape'
                                        ]}
-    st.plotly_chart(fig, use_container_width=True, height=height, config=config)
+    st.plotly_chart(fig, use_container_width=True, height=height) #, config=config)
     
     
 def get_core_feature(c1_IDs, c2_IDs, core_id):
