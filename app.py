a31588b4cc565148638b5b6a1aed7395ef3cd85b
xim2016 <xj_ma@yahoo.com>
2025-06-02 19:43:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,35 @@ from utils import get_screen_width
 from start_page import start_page
 from mobile.start_page_mobile import start_page_mobile
 
-# st.set_page_config(
-#         # layout='wide',
-#         page_title='Mesothelioma Spatial Atlas',
-#         page_icon="./assets/figures/meso_ribbon.png",
-#         # initial_sidebar_state="collapsed",
-# )
+st.set_page_config(
+        # layout='wide',
+        page_title='Mesothelioma Spatial Atlas',
+        page_icon="./assets/figures/meso_ribbon.png",
+        # initial_sidebar_state="collapsed",
+)
 
+max_width_str = f"max-width: {80}%;"
+st.markdown(f"""
+        <style>
+        .appview-container .main .block-container{{{max_width_str}}}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
 # st.elements.utils._shown_default_value_warning=True
+
+hide_default_format = """
+       <style>
+       .block-container {
+           padding-top: 0rem;
+       }
+       header[data-testid="stHeader"] {
+           display: none;
+       }
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
 
 width = get_screen_width()
 
