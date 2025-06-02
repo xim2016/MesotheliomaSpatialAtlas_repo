76a39399e3ebc28a35c9eaabd81b7cf47ca24035
xim2016 <xj_ma@yahoo.com>
2025-06-02 19:47:08 -0400

    Update start_page.py

--- start_page.py
+++ start_page.py
@@ -9,7 +9,7 @@ import hydralit_components as hc
 from style import page_style, footer
 
 st.set_page_config(
-        layout='wide',
+        # layout='wide',
         page_title='Mesothelioma Spatial Atlas',
         page_icon="./assets/figures/meso_ribbon.png",
         # initial_sidebar_state="collapsed"
