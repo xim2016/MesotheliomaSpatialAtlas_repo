0bfe4c316b85cfe663dec5e3c4d76c5562f09b15
xim2016 <xj_ma@yahoo.com>
2025-08-22 16:55:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,6 @@ from utils import get_screen_width
 from start_page import start_page
 from mobile.start_page_mobile import start_page_mobile
 
-# For Google analytics track
-import streamlit.components.v1 as components
-
-def inject_google_tag():
-    GA_JS = """
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-W2G8Q99WCS"></script>
-    <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-W2G8Q99WCS');
-    </script>
-    """
-    components.html(GA_JS, height=0)
-
-# Call the function at the start of your app
-inject_google_tag()
-# end of the track
-
-
 
 st.set_page_config(
         layout='wide',
