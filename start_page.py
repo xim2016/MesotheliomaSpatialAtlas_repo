680733cbca9d509f8823719139ee11ea83221e12
xim2016 <xj_ma@yahoo.com>
2025-10-01 18:53:30 -0400

    Update start_page.py

--- start_page.py
+++ start_page.py
@@ -13,13 +13,13 @@ import streamlit.components.v1 as components
 
 def inject_google_tag():
     GA_JS = """
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-W2G8Q99WCS"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GJ8KNPB8W8"></script>
     <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', 'G-W2G8Q99WCS');
+          gtag('config', 'G-GJ8KNPB8W8');
     </script>
     """
     st.write("")
